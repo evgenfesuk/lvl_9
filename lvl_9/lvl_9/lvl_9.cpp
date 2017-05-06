184ce6864fbb5ea7b78e3e5aaf0fc53a717b782c
evgen.fesuk <evgen.fesuk@gmail.com>
2017-05-06 22:13:22 +0300

    main in progress

--- lvl_9/lvl_9/lvl_9.cpp
+++ lvl_9/lvl_9/lvl_9.cpp
@@ -3,10 +3,38 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include "publication.h"
+#include "book.h"
 
 
 int main()
 {
+	std::cout << "Printed (b)ook or (a)udio?" << std::endl;
+	char answer;
+	float bookPrice;
+	std::string bookName;
+	unsigned int numberOfPages;
+	float minutes;
+	std::cin >> answer;
+
+	switch (answer)
+	{
+	case 'b':
+	{
+		book b1;
+		std::cout << "Input name, price and number of pages in book: " << std::endl;
+
+		break;
+	}
+	case 'a':
+	{
+
+		break;
+	}
+	default:
+		break;
+	}
+	system("pause");
     return 0;
 }
 
