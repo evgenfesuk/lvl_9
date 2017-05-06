56a787eda868ac752679075c2fe4398d83319fac
evgen.fesuk <evgen.fesuk@gmail.com>
2017-05-06 21:51:59 +0300

    class record created

---
+++ lvl_9/lvl_9/record.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include "record.h"
+
+
+record::record()
+{
+	publication();
+	minutes = 0;
+}
+
+record::record(std::string bN, float bP, float _minutes)
+{
+	publication(bN, bP);
+	minutes = _minutes;
+}
+
+
+record::~record()
+{
+}
+
+void record::set(std::string bN, float bP, float _minutes)
+{
+	publication(bN, bP);
+	minutes = _minutes;
+}
+
+void record::get() const
+{
+	publication::get();
+	std::cout << "Total minutes in book: " << minutes << std::endl;
+}
