72e4c6c3379f0fbfe32dd3e48af1445013737246
evgen.fesuk <evgen.fesuk@gmail.com>
2017-05-07 18:22:11 +0300

    Multiple Inheritance from publication and sales

--- lvl_9/lvl_9/record.cpp
+++ lvl_9/lvl_9/record.cpp
@@ -6,7 +6,7 @@ record::record() : publication(), minutes(0)
 {
 }
 
-record::record(std::string bN, float bP, float _minutes) : publication(bN, bP), minutes(_minutes)
+record::record(std::string bN, float bP, float _minutes, float m1, float m2, float m3) : publication(bN, bP), minutes(_minutes), sales(0, 0, 0)
 {
 }
 
@@ -19,10 +19,12 @@ void record::set(std::string bN, float bP, float _minutes)
 {
 	publication::set(bN, bP);
 	minutes = _minutes;
+	sales::set();
 }
 
 void record::get() const
 {
 	publication::get();
 	std::cout << "Total minutes in book: " << minutes << std::endl;
+	sales::get();
 }
--- lvl_9/lvl_9/record.h
+++ lvl_9/lvl_9/record.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "publication.h"
+#include "sales.h"
 class record :
-	protected publication
+	protected publication, protected sales
 {
 public:
 	record();
-	record(std::string bN, float bP, float _minutes);
+	record(std::string bN, float bP, float _minutes, float m1, float m2, float m3);
 	~record();
 	void set(std::string bN, float bP, float _minutes);
 	void get() const;
