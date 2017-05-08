9638d9edf370a8ccab3d67c7b3fa927a169bd10a
evgen.fesuk <evgen.fesuk@gmail.com>
2017-05-08 13:13:53 +0300

    deleted publication set/get from functions

--- lvl_9/lvl_9/disk.cpp
+++ lvl_9/lvl_9/disk.cpp
@@ -13,15 +13,14 @@ disk::~disk()
 
 void disk::get() const
 {
-	if(cd) std::cout << "\nDisk type is cd" << std::endl;
-	else std::cout << "Disk type is dvd" << std::endl;
-	publication::get();
+	if(cd) std::cout << "Disk type is: CD" << std::endl;
+	else std::cout << "Disk type is: DVD" << std::endl;
 }
 
 void disk::set(std::string bN, float bP)
 {
 	std::cout << "(c)d or (d)vd disk? " << std::endl;
-	if (getche()=='c')
+	if (_getche()=='c')
 	{
 		cd = true;
 		dvd = false;
@@ -31,5 +30,4 @@ void disk::set(std::string bN, float bP)
 		dvd = true;
 		cd = false;
 	}
-	publication::set(bN, bP);
 }
--- lvl_9/lvl_9/disk.h
+++ lvl_9/lvl_9/disk.h
@@ -1,7 +1,5 @@
 #pragma once
 #include "publication.h"
-#include "book.h"
-#include "record.h"
 #include <conio.h>
 class disk :
 	protected publication
