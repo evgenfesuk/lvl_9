93ae7d6b7b35e616a935f8e54200109c9d3c49b4
evgen.fesuk <evgen.fesuk@gmail.com>
2017-05-13 18:56:25 +0300

    changed publication to publication2

--- lvl_9/lvl_9/record.cpp
+++ lvl_9/lvl_9/record.cpp
@@ -2,11 +2,11 @@
 #include "record.h"
 
 
-record::record() : publication(), minutes(0), sales()
+record::record() : publication2(), minutes(0), sales()
 {
 }
 
-record::record(std::string bN, float bP, float _minutes, float m1, float m2, float m3) : publication(bN, bP), minutes(_minutes), sales(0, 0, 0)
+record::record(std::string bN, float bP, float _minutes, float m1, float m2, float m3, unsigned short _d, unsigned short _m, unsigned short _y) : publication2(bN, bP, _d, _m, _y), minutes(_minutes), sales(0, 0, 0)
 {
 }
 
@@ -15,9 +15,9 @@ record::~record()
 {
 }
 
-void record::set(std::string bN, float bP, float _minutes)
+void record::set(std::string bN, float bP, float _minutes, unsigned short _d, unsigned short _m, unsigned short _y)
 {
-	publication::set(bN, bP);
+	publication2::set(bN, bP, _d, _m, _y);
 	minutes = _minutes;
 	d.set(bN, bP);
 	sales::set();
@@ -25,7 +25,7 @@ void record::set(std::string bN, float bP, float _minutes)
 
 void record::get() const
 {
-	publication::get();
+	publication2::get();
 	std::cout << "Total minutes in book: " << minutes << std::endl;
 	d.get();
 	sales::get();
--- lvl_9/lvl_9/record.h
+++ lvl_9/lvl_9/record.h
@@ -1,15 +1,15 @@
 #pragma once
-#include "publication.h"
+#include "publication2.h"
 #include "sales.h"
 #include "disk.h"
 class record :
-	protected publication, protected sales
+	protected publication2, protected sales
 {
 public:
 	record();
-	record(std::string bN, float bP, float _minutes, float m1, float m2, float m3);
+	record(std::string bN, float bP, float _minutes, float m1, float m2, float m3, unsigned short _d, unsigned short _m, unsigned short _y);
 	~record();
-	void set(std::string bN, float bP, float _minutes);
+	void set(std::string bN, float bP, float _minutes, unsigned short _d, unsigned short _m, unsigned short _y);
 	void get() const;
 protected:
 	float minutes;
