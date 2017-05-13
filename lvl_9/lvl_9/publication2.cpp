17db32ef191432a5bb382de8a7e232b2da0dd533
evgen.fesuk <evgen.fesuk@gmail.com>
2017-05-13 20:02:47 +0300

    added comments

--- lvl_9/lvl_9/publication2.cpp
+++ lvl_9/lvl_9/publication2.cpp
@@ -4,14 +4,14 @@
 
 publication2::publication2()
 {
-	publication::publication();
-	iDate.set(0, 0, 0);
+	publication::publication(); // пустой родительский конструктор класса
+	iDate.set(0, 0, 0); // + дату устанавливаем на 0
 }
 
 publication2::publication2(std::string bN, float bP, unsigned short _d, unsigned short _m, unsigned short _y)
 {
-	publication::publication(bN, bP);
-	iDate.set(_d, _m, _y);
+	publication::publication(bN, bP); // родительский конструктор с параметрами
+	iDate.set(_d, _m, _y); // установка даты релиза
 }
 
 
@@ -21,11 +21,11 @@ publication2::~publication2()
 
 void publication2::set(std::string bN, float bP, unsigned short _d, unsigned short _m, unsigned short _y)
 {
-	publication::set(bN, bP);
-	iDate.set(_d, _m, _y);
+	publication::set(bN, bP); // родительская функция set
+	iDate.set(_d, _m, _y); // установка даты релиза
 }
 
-void publication2::get() const
+void publication2::get() const // вывод инфо на экран
 {
 	publication::get();
 	iDate.show();
--- lvl_9/lvl_9/publication2.h
+++ lvl_9/lvl_9/publication2.h
@@ -6,13 +6,22 @@ class publication2 :
 {
 public:
 	publication2();
+	/*
+	bN - имя книги
+	bP - цена книги
+	_numberOfPages - кол-во страниц в книге
+	m1 - продажи за первый месяц
+	m2 - продажи за второй месяц
+	m3 - продажи за третий месяц
+	_d - дата релиза, день
+	_m - дата релиза, месяц
+	_y - дата релиза, год
+	*/
 	publication2(std::string bN, float bP, unsigned short _d, unsigned short _m, unsigned short _y);
 	~publication2();
 	void set(std::string bN, float bP, unsigned short _d, unsigned short _m, unsigned short _y);
 	void get() const;
 protected:
-	/*float bookPrice;
-	std::string bookName;*/
-	date iDate;
+	date iDate; // дата релиза
 };
 
