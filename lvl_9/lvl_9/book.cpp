aa90b7faaf3b82be87ba8479b3e599e0186d8e35
evgen.fesuk <evgen.fesuk@gmail.com>
2017-05-13 18:49:55 +0300

    changed publication to publication2

--- lvl_9/lvl_9/book.cpp
+++ lvl_9/lvl_9/book.cpp
@@ -2,17 +2,17 @@
 #include "book.h"
 
 
-book::book() : publication(), numberOfPages(0), sales()
+book::book() : publication2(), numberOfPages(0), sales()
 {
 }
 
-book::book(std::string bN, float bP, int _numberOfPages, float m1, float m2, float m3) : publication(bN, bP), numberOfPages(_numberOfPages), sales(0, 0, 0)
+book::book(std::string bN, float bP, int _numberOfPages, float m1, float m2, float m3, unsigned short _d, unsigned short _m, unsigned short _y) : publication2(bN, bP, _d, _m, _y), numberOfPages(_numberOfPages), sales(0, 0, 0)
 {
 }
 
-void book::set(std::string bN, float bP, int _numberOfPages)
+void book::set(std::string bN, float bP, int _numberOfPages, unsigned short _d, unsigned short _m, unsigned short _y)
 {
-	publication::set(bN, bP);
+	publication2::set(bN, bP, _d, _m, _y);
 	numberOfPages = _numberOfPages;
 	d.set(bN, bP);
 	sales::set();
@@ -20,7 +20,7 @@ void book::set(std::string bN, float bP, int _numberOfPages)
 
 void book::get() const
 {
-	publication::get();
+	publication2::get();
 	std::cout << "Number of pages: " << numberOfPages << std::endl;
 	d.get();
 	sales::get();
--- lvl_9/lvl_9/book.h
+++ lvl_9/lvl_9/book.h
@@ -1,14 +1,14 @@
 #pragma once
-#include "publication.h"
+#include "publication2.h"
 #include "sales.h"
 #include "disk.h"
 class book :
-	protected publication, protected sales
+	public publication2, protected sales
 {
 public:
 	book();
-	book(std::string bN, float bP, int _numberOfPages, float m1, float m2, float m3);
-	void set(std::string bN, float bP, int _numberOfPages);
+	book(std::string bN, float bP, int _numberOfPages, float m1, float m2, float m3, unsigned short _d, unsigned short _m, unsigned short _y);
+	void set(std::string bN, float bP, int _numberOfPages, unsigned short _d, unsigned short _m, unsigned short _y);
 	void get() const;
 	~book();
 protected:
