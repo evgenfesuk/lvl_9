d1c56e5725f47d12ef00235b25a9ecc497e36af2
evgen.fesuk <evgen.fesuk@gmail.com>
2017-05-13 19:08:42 +0300

    changed type of input and modified checking of disk type for better quality

--- lvl_9/lvl_9/disk.cpp
+++ lvl_9/lvl_9/disk.cpp
@@ -14,20 +14,28 @@ disk::~disk()
 void disk::get() const
 {
 	if(cd) std::cout << "Disk type is: CD" << std::endl;
-	else std::cout << "Disk type is: DVD" << std::endl;
+	else if (dvd) std::cout << "Disk type is: DVD" << std::endl;
+	else std::cout << "Unknown disk type" << std::endl;
 }
 
 void disk::set(std::string bN, float bP)
 {
 	std::cout << "(c)d or (d)vd disk? " << std::endl;
-	if (_getche()=='c')
+	char answer;
+	answer = _getche();
+	if (answer=='c')
 	{
 		cd = true;
 		dvd = false;
 	}
-	else
+	else if (answer == 'd')
 	{
 		dvd = true;
+		cd = false;
+	}
+	else
+	{
+		dvd = false;
 		cd = false;
 	}
 }
