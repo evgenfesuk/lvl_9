8e39596fd3e202240b101f71d6d282f9850513c0
evgen.fesuk <evgen.fesuk@gmail.com>
2017-05-13 19:51:16 +0300

    added comments

--- lvl_9/lvl_9/book.cpp
+++ lvl_9/lvl_9/book.cpp
@@ -6,16 +6,17 @@ book::book() : publication2(), numberOfPages(0), sales()
 {
 }
 
-book::book(std::string bN, float bP, int _numberOfPages, float m1, float m2, float m3, unsigned short _d, unsigned short _m, unsigned short _y) : publication2(bN, bP, _d, _m, _y), numberOfPages(_numberOfPages), sales(0, 0, 0)
+book::book(std::string bN, float bP, int _numberOfPages, float _m1, float _m2, float _m3, unsigned short _d, unsigned short _m, unsigned short _y) : publication2(bN, bP, _d, _m, _y), numberOfPages(_numberOfPages), sales(_m1, _m2, _m3)
 {
+	// создание книги со всей информацией через конструктор
 }
 
 void book::set(std::string bN, float bP, int _numberOfPages, unsigned short _d, unsigned short _m, unsigned short _y)
 {
-	publication2::set(bN, bP, _d, _m, _y);
-	numberOfPages = _numberOfPages;
-	d.set(bN, bP);
-	sales::set();
+	publication2::set(bN, bP, _d, _m, _y); // ифнормация о названии книги, цене и дате релиза
+	numberOfPages = _numberOfPages; // кол-во страниц
+	d.set(); // устанавливаем тип диска
+	sales::set(); // указываем продажи за 3 месяца
 }
 
 void book::get() const
--- lvl_9/lvl_9/book.h
+++ lvl_9/lvl_9/book.h
@@ -3,16 +3,27 @@
 #include "sales.h"
 #include "disk.h"
 class book :
-	public publication2, protected sales
+	public publication2, protected sales // множественное наследование
 {
 public:
-	book();
-	book(std::string bN, float bP, int _numberOfPages, float m1, float m2, float m3, unsigned short _d, unsigned short _m, unsigned short _y);
+	book(); // пустой конструктор
+	book(std::string bN, float bP, int _numberOfPages, float _m1, float _m2, float _m3, unsigned short _d, unsigned short _m, unsigned short _y);
 	void set(std::string bN, float bP, int _numberOfPages, unsigned short _d, unsigned short _m, unsigned short _y);
+	/*
+	bN - имя книги
+	bP - цена книги
+	_numberOfPages - кол-во страниц в книге
+	m1 - продажи за первый месяц
+	m2 - продажи за второй месяц
+	m3 - продажи за третий месяц
+	_d - дата релиза, день
+	_m - дата релиза, месяц
+	_y - дата релиза, год
+	*/
 	void get() const;
 	~book();
 protected:
 	unsigned int numberOfPages;
-	disk d;
+	disk d; // диск для книги
 };
 
