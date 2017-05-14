688b395bcedb073b505a2b98021953ae893d889e
evgen.fesuk <evgen.fesuk@gmail.com>
2017-05-14 21:11:14 +0300

    modified set function and added new constructor

--- lvl_9/lvl_9/sales.cpp
+++ lvl_9/lvl_9/sales.cpp
@@ -18,14 +18,32 @@ sales::sales(float _m1, float _m2, float _m3) // инициалиация полученными значен
 	totalSaled[2] = _m3;
 }
 
+sales::sales(float *array, int size)
+{
+	float * ptr;
+	ptr = array;
+	for (int i = 0; i < size; i++)
+	{
+		totalSaled[i] = ptr[i]; 
+	}
+}
+
 sales::~sales()
 {
 }
 
 void sales::set() // инициалиация полученными значениями
 {
-	std::cout << "Input information about sales for last 3 month" << std::endl;
+	/*std::cout << "Input information about sales for last 3 month" << std::endl;
 	for (int i = 0; i < 3; i++)
+	{
+		std::cin >> totalSaled[i];
+	}*/
+
+	std::cout << "Input number of month" << std::endl;
+	std::cin >> numberOfMonth;
+	std::cout << "Input information about sales for last " << numberOfMonth << " month" << std::endl;
+	for (int i = 0; i < numberOfMonth; i++)
 	{
 		std::cin >> totalSaled[i];
 	}
--- lvl_9/lvl_9/sales.h
+++ lvl_9/lvl_9/sales.h
@@ -5,6 +5,7 @@ class sales
 public:
 	sales();
 	sales(float _m1, float _m2, float _m3);
+	sales(float *, int);
 	/*
 	m1 - продажи за первый мес€ц
 	m2 - продажи за второй мес€ц
@@ -15,5 +16,6 @@ public:
 	void get() const;
 protected:
 	float totalSaled[3]; // массив продаж по мес€цам
+	int numberOfMonth;
 };
 
